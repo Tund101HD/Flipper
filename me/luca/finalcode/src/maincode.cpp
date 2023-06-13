ba54c23e9b5fdc90b002403a5bc1dfd72b120a0e
Tund101HD <127756014+Tund101HD@users.noreply.github.com>
2023-06-13 20:22:09 +0200

    Updating code
    Es wird kein Startknopf mehr benötigt. Das Spiel startet nun automatisch nach dem Geldeinwurf.
    Desweiteren wurde ein Bug bei der Übermittlung der übrigen Games behoben.

--- me/luca/finalcode/src/maincode.cpp
+++ me/luca/finalcode/src/maincode.cpp
@@ -15,34 +15,34 @@ boolean check_game_over();
 void transmit_inst(byte b);
 void transmit_aryInst(byte b[2], byte inst);
 int GAMES_LEFT;
+const long One_Second_Pause = 1000;
+const long Half_Second_Pause = 500;
+unsigned long previousMillis = 0;
 
 
 void setup() {
-    Wire.begin(); 
+    Wire.begin();
     Serial.begin(9600);
 
 
     pinMode(3, INPUT_PULLUP); //IR 1
     pinMode(4, INPUT_PULLUP); //IR 2
-    pinMode(5, INPUT_PULLUP); //IR 3
-    pinMode(6, INPUT_PULLUP); //Start Knopf
+    pinMode(5, INPUT_PULLUP); //Game-Over IR
 
+
+    pinMode(6, INPUT_PULLUP); //Transmit-Due-Games pin
     servo.attach(9, 440, 1900);
-    pinMode(10, INPUT_PULLUP); //TODO placeholder Game-Over IR
     pinMode(13, OUTPUT); //Sneaky workaround pin
     digitalWrite(13, HIGH);
 }
 
 
 void loop() {
-
     while(!GAMESTATUS){
         GAMESTATUS = check_start_init();
         delay(10);
     }
-
     GAMESTATUS = !check_game_over();
-
 }
 
 
@@ -52,32 +52,51 @@ boolean check_start_init(){
 
     // boolean money = digitalRead(3) == 0 || digitalRead(4) == 0 || digitalRead(5) == 0; -> Alter Code, der nicht auf Geldmenge achtet
     if(digitalRead(3) == 0){ // 50 CENT
-        money = true;
+        if(!money)
+            money = true;
         GAMES_LEFT+=1;
     }
     if(digitalRead(4) == 0){ // 1 EUR
-        money = true;
+        if(!money)
+            money = true;
         GAMES_LEFT+=2;
     }
-    if(digitalRead(5) == 0){ // 2 EUR
-        money = true;
-        GAMES_LEFT+=4;
-    }
 
-    boolean startbtn = digitalRead(6) == 0;
 
-    if(money && startbtn){
+    if(money){
+        delay(2000);
         for (int i = 0; i < 91; ++i) { //Ballarm
             servo.write(i);
+
+            if(digitalRead(3) == 0){ // 50 CENT
+                GAMES_LEFT+=1;
+            }
+            if(digitalRead(4) == 0){ // 1 EUR
+                GAMES_LEFT+=2;
+            }
         }
         servo.write(0);
+        if(GAMES_LEFT >=255)
+            GAMES_LEFT = 255;
+        previousMillis = millis();
+        while(previousMillis < 2*One_Second_Pause){
+            if(digitalRead(3) == 0){ // 50 CENT
+                GAMES_LEFT+=1;
+            }
+            if(digitalRead(4) == 0){ // 1 EUR
+                GAMES_LEFT+=2;
+            }
+        }
+
         byte instruction[2] = {(byte) 0, (byte) GAMES_LEFT};
         transmit_aryInst(instruction, start);
-
         Serial.println("[Log] Starting Game!");
         GAMES_LEFT--;
         digitalWrite(13, LOW);
         return true;
+    }
+    if(digitalRead(6) == HIGH){
+        transmit_inst((byte) GAMES_LEFT);
     }
 
 
@@ -86,9 +105,21 @@ boolean check_start_init(){
 
 
 boolean check_game_over(){
-   boolean gmovr = digitalRead(10) == 0;
+   if(digitalRead(6) == HIGH){
+       transmit_inst((byte) GAMES_LEFT);
+   }
+   boolean gmovr = digitalRead(5) == 0;
    if(!gmovr)
        return false;
+
+    if(digitalRead(3) == 0){ // 50 CENT
+        GAMES_LEFT+=1;
+    }
+    if(digitalRead(4) == 0){ // 1 EUR
+        GAMES_LEFT+=2;
+    }
+    if(GAMES_LEFT >=255)
+        GAMES_LEFT = 255;
 
     byte inst[2] = {stop, (byte) GAMES_LEFT};
     transmit_aryInst(inst, stop);
@@ -98,7 +129,7 @@ boolean check_game_over(){
 }
 
 
-void transmit_inst(byte b){ //Einzelübertragung (Deprecated)
+void transmit_inst(byte b){
     Wire.beginTransmission(1);
     Wire.write(b);
     Wire.endTransmission();
