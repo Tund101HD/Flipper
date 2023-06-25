25e8c6d839d38a46810b16c5a8c81f2387f9cbff
Tund101HD <127756014+Tund101HD@users.noreply.github.com>
2023-06-25 16:44:49 +0200

    Code update 3

--- me/luca/finalcode/src/maincode.cpp
+++ me/luca/finalcode/src/maincode.cpp
@@ -1,65 +1,63 @@
 #include <Arduino.h>
 #include "Servo.h"
-#include "SD.h"
-#include "SPI.h"
 #include "Wire.h"
 
-const byte start = 0;
-const byte stop = 1;
 Servo servo;
 
 boolean GAMESTATUS = false;
-boolean check_start_init();
-boolean check_game_over();
+int GAMES_LEFT = 0;
+
+const byte start = 0;
+const byte stop = 1;
+const long One_Second_Pause = 1000;
+const long Half_Second_Pause = 500;
+unsigned long previousMillis = 0;
 
 void transmit_inst(byte b);
-void transmit_aryInst(byte b[], byte inst);
-int GAMES_LEFT = 0;
-int GAMES_NO = 0;
-
+void transmit_aryInst(byte b[2], byte inst);
+boolean start_phase();
+boolean ingame_phase();
 
 void setup() {
-    Wire.begin(); 
     Serial.begin(9600);
-
+    Wire.begin();
 
     pinMode(3, INPUT_PULLUP); //IR 1
     pinMode(4, INPUT_PULLUP); //IR 2
-    pinMode(5, INPUT_PULLUP); //Game Over IR
-    pinMode(6, INPUT_PULLUP); //Transmit Due games
+    pinMode(5, INPUT_PULLUP); //Game-Over IR
+
 
     servo.attach(9, 440, 1900);
+    servo.write(0);
     pinMode(13, OUTPUT); //Sneaky workaround pin
     digitalWrite(13, HIGH);
 }
 
-
 void loop() {
-
-    while(!GAMESTATUS){
-        GAMESTATUS = check_start_init();
-        delay(10);
+    while (!GAMESTATUS){
+        GAMESTATUS = start_phase();
+        delay(15);
     }
-
-    GAMESTATUS = !check_game_over();
-
+        GAMESTATUS = ingame_phase();
+    delay(15);
 }
 
 
 boolean check_start_init(){
     boolean money = GAMES_LEFT > 0;
-    Serial.println("[Log] Games left: " + String(GAMES_LEFT));
-    // boolean money = digitalRead(3) == 0 || digitalRead(4) == 0 || digitalRead(5) == 0; -> Alter Code, der nicht auf Geldmenge achtet
     if(digitalRead(3) == 0){ // 50 CENT
-        money = true;
+        if(!money)
+            money = true;
         GAMES_LEFT+=1;
     }
     if(digitalRead(4) == 0){ // 1 EUR
-        money = true;
+        if(!money)
+            money = true;
         GAMES_LEFT+=2;
     }
 
     if(money){
+        /*FIXME Zwei Sekunden Delay */
         previousMillis = millis();
         while(millis()- previousMillis < 2*One_Second_Pause){
             if(digitalRead(3) == 0){ // 50 CENT
@@ -68,44 +66,65 @@ boolean check_start_init(){
             if(digitalRead(4) == 0){ // 1 EUR
                 GAMES_LEFT+=2;
             }
+            delay(10);
         }
-        for (int i = 0; i < 91; ++i) { //Ballarm
-            servo.write(i);
-        }
+
+        /*FIXME Servo move phase*/
         servo.write(0);
+        for(int i = 0; i<90; i++){
+            servo.write(i);
+
+            previousMillis = millis();
+            while(millis() - previousMillis < 2){
+                if(digitalRead(3) == 0){ // 50 CENT
+                    GAMES_LEFT+=1;
+                }
+                if(digitalRead(4) == 0){ // 1 EUR
+                    GAMES_LEFT+=2;
+                }
+            }
+            delay(9);
+        }
+        for(int i = 90; i>0; i--){
+            servo.write(i);
+            previousMillis = millis();
+
+        }
+
         byte instruction[2] = {(byte) 0, (byte) GAMES_LEFT};
         transmit_aryInst(instruction, start);
-
-        Serial.println("[Log] Starting Game! Prepay: "+ String(GAMES_LEFT));
+        /*FIXME Com phase without wire!*/
+        Serial.println("[Log] Starting Game!");
         GAMES_LEFT--;
-        GAMES_NO++;
         digitalWrite(13, LOW);
         return true;
     }
     return false;
 }
 
-
 boolean check_game_over(){
-    if(digitalRead(6) == HIGH){
-        noInterrupts();
-        byte packet[3] = {start, (byte) GAMES_LEFT, (byte) GAMES_NO};
-        transmit_aryInst(packet, start);
-        interrupts();
+    if(!GAMESTATUS)
+        return false;
+
+    boolean gmovr = digitalRead(5) == 0;
+    if(!gmovr){
+        if(digitalRead(3) == 0){ // 50 CENT
+            GAMES_LEFT+=1;
+        }
+        if(digitalRead(4) == 0){ // 1 EUR
+            GAMES_LEFT+=2;
+        }
+        return true;
     }
-   boolean gmovr = digitalRead(5) == 0;
-   if(!gmovr)
-       return false;
 
     byte inst[2] = {stop, (byte) GAMES_LEFT};
     transmit_aryInst(inst, stop);
     Serial.println("[Log] Ending Game!");
     digitalWrite(13, HIGH);
-    return true;
+    return false;
 }
 
-
-void transmit_inst(byte b){ //Einzelübertragung (Deprecated)
+void transmit_inst(byte b){
     Wire.beginTransmission(1);
     Wire.write(b);
     Wire.endTransmission();
@@ -115,7 +134,7 @@ void transmit_inst(byte b){ //Einzelübertragung (Deprecated)
 }
 
 
-void transmit_aryInst(byte b[], byte inst){
+void transmit_aryInst(byte b[2], byte inst){
     Wire.beginTransmission(1);
     Wire.write(b, sizeof(b));
     Wire.endTransmission();
